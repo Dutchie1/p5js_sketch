77f876e07912aee1aefcf8da36ce9694d3ff1250
Dutchie1 <138634491+Dutchie1@users.noreply.github.com>
2023-09-19 22:25:06 -0300

    Add files via upload

---
+++ projeto1/sketch.js
@@ -0,0 +1,49 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function setup() {
+
+createCanvas(600, 600);
+
+}
+
+function draw() {
+
+background("black");
+
+stroke("blue") // alterando a cor da borda do desenho
+
+fill("red") // alterando a cor do preenchimento do desenho
+
+rect(100, 250, 100, 150)
+
+}
+
+function draw() {
+  background(220);
+
+}
+
+
+function setup() {
+
+createCanvas(600, 600);
+
+background("black");
+
+}
+
+function draw() {
+
+stroke("blue");
+
+fill("red");
+
+if(mouseIsPressed){
+
+rect(mouseX, mouseY, 20, 35);
+
+}
+
+}
